91b8db22680eb9e6635c853878459d86e10b7d73
parsanojavan <parsa.nojavan85@gmail.com>
2026-08-02 02:27:29 +0330

    feat(chat): add direct creation

---
+++ src/direct/direct.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { DirectService } from './direct.service';
+import { DirectController } from './direct.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import Room, { RoomSchema } from 'src/models/concrete/room';
+import RoomMember, { RoomMemberSchema } from 'src/models/concrete/member';
+import Message, { MessageSchema } from 'src/models/concrete/message';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Room.name, schema: RoomSchema },
+      { name: RoomMember.name, schema: RoomMemberSchema },
+      { name: Message.name, schema: MessageSchema },
+    ]),
+  ],
+  providers: [DirectService],
+  controllers: [DirectController]
+})
+export class DirectModule { }
