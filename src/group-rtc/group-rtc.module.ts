f09f5f761b7d2a528042cc41c1cfe39e13dc664c
parsanojavan <parsa.nojavan85@gmail.com>
2026-08-27 01:42:26 +0330

    feat(chat): add livekit token creation

--- src/group-rtc/group-rtc.module.ts
+++ src/group-rtc/group-rtc.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
 import { GroupRtcController } from './group-rtc.controller';
 import { GroupRtcService } from './group-rtc.service';
-import { GroupRtcGateway } from './group-rtc.gateway';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Room } from 'livekit-server-sdk';
+import RoomMember, { RoomMemberSchema } from 'src/models/concrete/member';
+import Message, { MessageSchema } from 'src/models/concrete/message';
+import Reaction, { ReactionSchema } from 'src/models/concrete/reaction';
+import { RoomSchema } from 'src/models/concrete/room';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Room.name, schema: RoomSchema },
+      { name: RoomMember.name, schema: RoomMemberSchema },
+      { name: Message.name, schema: MessageSchema },
+      { name: Reaction.name, schema: ReactionSchema }
+    ]),
+  ],
   controllers: [GroupRtcController],
-  providers: [GroupRtcService, GroupRtcGateway]
+  providers: [GroupRtcService]
 })
-export class GroupRtcModule {}
+export class GroupRtcModule { }
